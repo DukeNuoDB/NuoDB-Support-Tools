9e37bfa67bc1110b1a463af9aa7ad1b7c2183a09
DukeNuoDB <dadamonis@nuodb.com>
2015-02-05 16:56:08 -0500

    Update cheetah.py
    Style / readability improvements (pep8).

--- cheetah.py
+++ cheetah.py
@@ -2,11 +2,15 @@ from sys import argv
 from collections import OrderedDict
 
 # Find and catalog each log line that matches these strings
-match_strings = ["NuoAgent version","Assertion", "error", "failed", "INFO", "WARN", "SEVERE","Local node","Remote Node",
-"minorty partition","heartbeat expired", "evict", "DIED","exit code", "Database is inactive",
-"Node joined", "Node left"]
-# Don't print out every line that contains these strings, instead trucnate to the last N lines
-summarize = ["error","INFO", "WARN", "failed"]
+match_strings = [
+	"NuoAgent version", "Assertion", "error", "failed", "INFO", "WARN",
+	"SEVERE", "Local node", "Remote Node", "minorty partition",
+	"heartbeat expired", "evict", "DIED", "exit code",
+	"Database is inactive", "Node joined", "Node left"
+]
+# Don't print out every line that contains these strings,
+# instead trucnate to the last N lines
+summarize = ["error", "INFO", "WARN", "failed"]
 summarize_length = 5
 
 if len(argv) > 1:
@@ -16,29 +20,30 @@ else:
 	print "Example:"
 	print "%s my.log" % argv[0]
 	exit(2)
-	
+
 for filename in files:
 	with open(filename) as f:
 		data = f.read().splitlines()
-		
+
 		# Create data structure to handle results
 		matches = OrderedDict()
 		for string in match_strings:
 			matches[string] = []
-			
+
 		for i, s in enumerate(data, 1):
 			for string in match_strings:
 				if string in s:
-					matches[string].append('Line %03d: %s' % (i,s,))
-		
+					matches[string].append('Line %03d: %s' % (i, s,))
+
 		# There are always more INFO and WARN messages than are usable.
 		# This returns the  most recent messages for each.
 		for item in summarize:
-			matches["%s (last %d lines)" % (item, summarize_length)] = matches[item][(summarize_length * -1):]
-		
+			matches[
+				"%s (last %d lines)" % (item, summarize_length)] = matches[item]
+			[(summarize_length * -1):
+		]
 
 		nodemerge = matches['Node joined'] + matches['Node left']
-		#nodemerge.sort()
 		matches['nodes'] = nodemerge
 		del matches['Node joined']
 		del matches['Node left']
@@ -57,12 +62,11 @@ for filename in files:
 	print "======================================"
 	print "==Message Details"
 	print "======================================"
-	for string in matches:	
+	for string in matches:
 		if string not in summarize:
 			print "%s:" % string.upper()
 			if len(matches[string]) > 0:
 				print '\n'.join([str(myelement) for myelement in matches[string]])
 			else:
-				#print "No %s entries in the log" % string.upper()
 				print "0 entries in the log."
 			print
